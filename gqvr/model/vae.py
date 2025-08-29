bc00d05d6e5ba0ed0c947aafd60d46f208c29f1e
Aryan-Garg <aryangarg019@gmail.com>
2025-08-29 21:51:03 +0000

    Inflate with 3D temporal Convs before each 2D sp attn + output

--- gqvr/model/vae.py
+++ gqvr/model/vae.py
@@ -31,7 +31,7 @@ class Upsample(nn.Module):
             )
 
     def forward(self, x):
-        x = torch.nn.functional.interpolate(x, scale_factor=2.0, mode="nearest")
+        x = torch.nn.functional.interpolate(x, scale_factor=2, mode="bilinear", align_corners=False, antialias=True)
         if self.with_conv:
             x = self.conv(x)
         return x
@@ -864,3 +864,174 @@ class ConvEMA_AutoencoderKL(nn.Module):
             z = posterior.mode()
         dec = self.decode(z)
         return dec, posterior
+
+# Attempt 3 - Go 3D convs for IP Lifting
+class Decoder_3D(nn.Module):
+    def __init__(
+        self,
+        *,
+        ch,
+        out_ch,
+        ch_mult=(1, 2, 4, 8),
+        num_res_blocks,
+        attn_resolutions,
+        dropout=0.0,
+        resamp_with_conv=True,
+        in_channels,
+        resolution,
+        z_channels,
+        give_pre_end=False,
+        tanh_out=False,
+        use_linear_attn=False,
+        **ignorekwargs,
+    ):
+        super().__init__()
+        ### setup attention type
+        if Config.attn_mode == AttnMode.SDP:
+            attn_type = "sdp"
+        elif Config.attn_mode == AttnMode.XFORMERS:
+            attn_type = "xformers"
+        else:
+            attn_type = "vanilla"
+        if use_linear_attn:
+            attn_type = "linear"
+        self.ch = ch
+        self.temb_ch = 0
+        self.num_resolutions = len(ch_mult)
+        self.num_res_blocks = num_res_blocks
+        self.resolution = resolution
+        self.in_channels = in_channels
+        self.give_pre_end = give_pre_end
+        self.tanh_out = tanh_out
+
+        # compute in_ch_mult, block_in and curr_res at lowest res
+        in_ch_mult = (1,) + tuple(ch_mult)
+        block_in = ch * ch_mult[self.num_resolutions - 1]
+        curr_res = resolution // 2 ** (self.num_resolutions - 1)
+        self.z_shape = (1, z_channels, curr_res, curr_res)
+
+        # z to block_in
+        self.conv_in = torch.nn.Conv2d(
+            z_channels, block_in, kernel_size=3, stride=1, padding=1
+        )
+
+        # middle
+        self.mid = nn.Module()
+        self.mid.block_1 = ResnetBlock(
+            in_channels=block_in,
+            out_channels=block_in,
+            temb_channels=self.temb_ch,
+            dropout=dropout,
+        )
+        self.temp_mid_block_1 = nn.Conv3d(
+            in_channels=block_in,
+            out_channels=block_in,
+            kernel_size=(3,1,1), padding=(1,0,0), groups=block_in
+        ) 
+        self.mid.attn_1 = make_attn(block_in, attn_type=attn_type)
+        self.mid.block_2 = ResnetBlock(
+            in_channels=block_in,
+            out_channels=block_in,
+            temb_channels=self.temb_ch,
+            dropout=dropout,
+        )
+        self.temp_mid_block_2 = nn.Conv3d(
+            in_channels=block_in,
+            out_channels=block_in,
+            kernel_size=(3,1,1), padding=(1,0,0), groups=block_in
+        ) 
+
+        # upsampling
+        self.up = nn.ModuleList()
+        for i_level in reversed(range(self.num_resolutions)):
+            block = nn.ModuleList()
+            attn = nn.ModuleList()
+            temp = nn.ModuleList()
+            block_out = ch * ch_mult[i_level]
+            for i_block in range(self.num_res_blocks + 1):
+                block.append(
+                    ResnetBlock(
+                        in_channels=block_in,
+                        out_channels=block_out,
+                        temb_channels=self.temb_ch,
+                        dropout=dropout,
+                    )
+                )
+                block_in = block_out
+                if curr_res in attn_resolutions:
+                    temp.append(nn.Conv3d(block_in, block_in, kernel_size=(3,1,1), padding=(1,0,0), groups=block_in))
+                    attn.append(make_attn(block_in, attn_type=attn_type))
+            up = nn.Module()
+            up.block = block
+            up.temp = temp
+            up.attn = attn
+            if i_level != 0:
+                up.upsample = Upsample(block_in, resamp_with_conv)
+                curr_res = curr_res * 2
+            self.up.insert(0, up)  # prepend to get consistent order
+
+        # end
+        self.norm_out = Normalize(block_in)
+        self.conv_out = torch.nn.Conv2d(
+            block_in, out_ch, kernel_size=3, stride=1, padding=1
+        )
+        self.temp_conv3d_out = nn.Conv3d(out_ch, out_ch, kernel_size=(3,1,1), padding=(1,0,0), groups=out_ch)
+
+    def forward(self, z):
+        # assert z.shape[1:] == self.z_shape[1:]
+        self.last_z_shape = z.shape
+
+        # timestep embedding
+        temb = None
+
+        # z to block_in
+        h = self.conv_in(z)
+
+        # middle
+        h = self.mid.block_1(h, temb)
+        ###############
+        h = h.view(1, -1, h.size(1), h.size(2), h.size(3)) # 1, T, C, H, W
+        h = self.temp_mid_block_1(h)
+        h = h.view(-1, h.size(2), h.size(3), h.size(4))
+        ###############
+        h = self.mid.attn_1(h)
+        
+        h = self.mid.block_2(h, temb)
+        ###############
+        h = h.view(1, -1, h.size(1), h.size(2), h.size(3)) # 1, T, C, H, W
+        h = self.temp_mid_block_2(h)
+        h = h.view(-1, h.size(2), h.size(3), h.size(4))
+        ###############
+
+        # upsampling
+        for i_level in reversed(range(self.num_resolutions)):
+            for i_block in range(self.num_res_blocks + 1):
+                h = self.up[i_level].block[i_block](h, temb)
+                if len(self.up[i_level].attn) > 0:
+                    ###############
+                    h = h.view(1, -1, h.size(1), h.size(2), h.size(3))
+                    h = self.up[i_level].temp[i_block](h)
+                    h = h.view(-1, h.size(2), h.size(3), h.size(4))
+                    ###############
+                    h = self.up[i_level].attn[i_block](h)
+
+            if i_level != 0:
+                h = self.up[i_level].upsample(h)
+
+        # end
+        if self.give_pre_end:
+            return h
+
+        h = self.norm_out(h)
+        h = nonlinearity(h)
+        h = self.conv_out(h)
+
+        ###############
+        h = h.view(1, -1, h.size(1), h.size(2), h.size(3)) # 1, T, C, H, W
+        h = self.temp_conv3d_out(h)
+        h = h.view(-1, h.size(2), h.size(3), h.size(4))
+        ###############
+
+        if self.tanh_out:
+            h = torch.tanh(h)
+        return h
